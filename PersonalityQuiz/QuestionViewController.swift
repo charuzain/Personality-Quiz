771378f12e5965e1809040460234f0ab7ff319c6
charuzain <charuabhizain@gmail.com>
2022-10-15 14:09:13 -0400

    Add question property

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -3,11 +3,53 @@
 //  PersonalityQuiz
 //
 //  Created by Charu Jain on 10/15/22.
-//
+// Will hold the array of Questions objects in property called questions
 
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    var questions : [Question] = [
+        Question(
+            text: "Which food do you like the most?",
+            type: .single,
+            answers: [
+                Answer(text: "Steak", type: .dog),
+                Answer(text: "Fish", type: .cat),
+                Answer(text: "Carrots", type: .rabbit),
+                Answer(text: "Corn", type: .turtle)
+            ]
+        ),
+        
+        Question(
+            text: "Which activities do you enjoy the most?",
+            type: .multiple,
+            answers: [
+                Answer(text: "Eating", type: .dog),
+                Answer(text: "Sleeping", type: .cat),
+                Answer(text: "Cuddling", type: .rabbit),
+                Answer(text: "Swimming", type: .turtle)
+            ]
+        ),
+        
+        Question(
+            text: "How much do you enjoy the car ride?",
+            type: .ranged,
+            answers: [
+                Answer(text: "I dislike them", type: .cat),
+                Answer(text: "I get little nervous", type: .rabbit),
+                Answer(text: "I love them", type: .dog),
+                Answer(text: "Barely notice them", type: .turtle)
+            ]
+        )
+        
+    ]
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
