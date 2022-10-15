a6129ddc08da6c1d592f242e16b23e937ed02743
charuzain <charuabhizain@gmail.com>
2022-10-15 19:32:29 -0400

    Pass Data to Result View

--- PersonalityQuiz/ResultViewController.swift
+++ PersonalityQuiz/ResultViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    var responses: [Answer]
+    
+    init?(coder:NSCoder, responses:[Answer]){
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
