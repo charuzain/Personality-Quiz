189077a8907db694e55a91f48982f648e955ff50
charuzain <charuabhizain@gmail.com>
2022-10-15 21:32:24 -0400

    Result displayed

--- PersonalityQuiz/ResultViewController.swift
+++ PersonalityQuiz/ResultViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
     var responses: [Answer]
     
     init?(coder:NSCoder, responses:[Answer]){
@@ -22,10 +27,30 @@ class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculatePersonalityResult()
 
         // Do any additional setup after loading the view.
     }
-    
+    func  calculatePersonalityResult(){
+        let frequencyOfAnswers = responses.reduce(into: [:]){
+            (counts , answer)in
+            counts[answer.type , default: 0] += 1
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        {(pair1, pair2) in
+        return pair1.value > pair2.value
+        })
+        
+//        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted {$0.1 > $1.1}.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
+        
+    }
 
     /*
     // MARK: - Navigation
